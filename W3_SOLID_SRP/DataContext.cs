75a716b1627b9337ed3a2316e0b27db2a6cec40c
mmcarthey <mmcarthey@L11601148249>
2025-09-04 21:12:35 -0500

    Refactored to use a Context

---
+++ W3_SOLID_SRP/DataContext.cs
@@ -0,0 +1,79 @@
+﻿namespace W2_EnhancedFileIO
+{
+
+    public class DataContext
+    {
+        public List<Character> Characters { get; set; }
+
+        private string _fileName = "input.csv";
+
+        public DataContext()
+        {
+            Characters = new List<Character>();
+        }
+
+        public void Read()
+        {
+            using (StreamReader reader = new StreamReader(_fileName))
+            {
+                reader.ReadLine();
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    string name = null;
+                    string restOfLine = line;
+
+                    if (line.IndexOf('"') >= 0)
+                    {
+                        var remainder = line.TrimStart('"'); // remove leading quote
+                        var quoteIndex = remainder.IndexOf('"'); // find next quote
+                        name = remainder.Substring(0, quoteIndex); // extract name
+                        restOfLine = remainder.Substring(quoteIndex + 2); // skip quote and comma
+                    }
+                    var cols = restOfLine.Split(",");
+
+                    var profession = cols[0];
+                    var level = cols[1];
+                    var hp = cols[2];
+                    var equip = cols[3];
+
+                    var character = new Character();
+                    character.Name = name;
+                    character.Profession = profession;
+                    character.Level = int.Parse(level);
+                    character.HitPoints = int.Parse(hp);
+
+                    var equipment = equip.Split("|");
+                    foreach (var eq in equipment)
+                    {
+                        character.Equipment.Add(eq);
+                    }
+
+                    Characters.Add(character);
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        public void Write(Character character)
+        {
+            Characters.Add(character);
+        }
+
+        public void SaveChanges()
+        {
+            using (StreamWriter writer = new StreamWriter(_fileName))
+            {
+                writer.WriteLine("Name,Profession,Level,HitPoints,Equipment");
+                foreach (var character in Characters)
+                {
+                    var equipment = string.Join("|", character.Equipment);
+                    var line = $"\"{character.Name}\",{character.Profession},{character.Level},{character.HitPoints},{equipment}";
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
--- W3_SOLID_SRP/Program.cs
+++ W3_SOLID_SRP/Program.cs
@@ -5,51 +5,20 @@ class Program
 
     static void Main(string[] args)
     {
-        var dataManager = new DataManager();
-        dataManager.Read();
-        DisplayMenu();
+        // Load the data
+        var context = new DataContext();
+        context.Read();
 
+        // Display menu
+        DisplayMenu();
         var userInput = Console.ReadLine();
-        Console.WriteLine($"You chose: {userInput}");
 
         if (userInput == "1")
         {
             // Display characters
-            foreach (var line in dataManager.FileContents)
+            foreach (var character in context.Characters)
             {
-                string name = null;
-                string restOfLine = line;
-
-                if (line.IndexOf('"') >= 0)
-                {
-                    var remainder = line.TrimStart('"'); // remove leading quote
-                    var quoteIndex = remainder.IndexOf('"'); // find next quote
-                    name = remainder.Substring(0, quoteIndex); // extract name
-                    restOfLine = remainder.Substring(quoteIndex + 2); // skip quote and comma
-                }
-                var cols = restOfLine.Split(",");
-
-                var profession = cols[0];
-                var level = cols[1];
-                var hp = cols[2];
-                var equip = cols[3];
-
-                var character = new Character();
-                character.Name = name;
-                character.Profession = profession;
-                character.Level = int.Parse(level);
-                character.HitPoints = int.Parse(hp);
-
-                var equipment = equip.Split("|");
-                foreach (var eq in equipment)
-                {
-                    character.Equipment.Add(eq);
-                }
-
-                //character.DisplayCharacter(character);
-
                 Console.WriteLine($"Character is {character}");
-
             }
         }
         else if (userInput == "2")
@@ -64,7 +33,7 @@ class Program
             newCharacter.Equipment.Add("sword");
             newCharacter.Equipment.Add("backpack");
 
-            dataManager.Write(newCharacter);
+            context.Write(newCharacter);
 
         }
         else if (userInput == "3")
@@ -74,13 +43,13 @@ class Program
         else if (userInput == "4")
         {
             // Find character
-            var results = dataManager.FileContents.OrderByDescending(c=>c);
+            //var results = dataManager.FileContents.OrderByDescending(c=>c);
 
             //Console.WriteLine(results);
-            foreach (var r in results)
-            {
-                Console.WriteLine(r);
-            }
+            //foreach (var r in results)
+            //{
+            //    Console.WriteLine(r);
+            //}
         }
         else
         {
