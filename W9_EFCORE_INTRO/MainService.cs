62542c8a0eada500a1186f2c0d39ccaa02024c00
Mark McArthey <mmcarthey@wctc.edu>
2025-10-23 18:22:33 -0500

    Added stub for W9 In-Class

---
+++ W9_EFCORE_INTRO/MainService.cs
@@ -0,0 +1,10 @@
+﻿namespace W9_EFCORE_INTRO
+{
+    public class MainService : IMainService
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Entering MainService");
+        }
+    }
+}
---
+++ W9_EFCORE_INTRO/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace W9_EFCORE_INTRO;
+
+public static class Startup
+{
+    public static IServiceCollection ConfigureServices()
+    {
+        IServiceCollection services = new ServiceCollection();
+
+        // Register services and dependencies
+        services.AddTransient<IMainService, MainService>();
+        services.AddSingleton<Program>();
+
+        return services;
+    }
+}
+
